d28536c4ba5e8282e1cb18aaf2cc7cc4909b7751
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2022-05-23 15:57:48 +0300

    add keyCode ( ENTER tab )

--- src/app/components/common/form/form.jsx
+++ src/app/components/common/form/form.jsx
@@ -35,7 +35,15 @@ const FormComponent = ({
             validate(data);
         }
     }, [data]);
-
+    const handleKeyDown = useCallback((event) => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            const form = event.target.form;
+            const indexField = Array.prototype.indexOf.call(form, event.target);
+            form.elements[indexField + 1].focus();
+        }
+        // console.log(event);
+    }, []);
     const isValid = Object.keys(errors).length === 0;
 
     const clonedElements = React.Children.map(children, (child) => {
@@ -54,7 +62,8 @@ const FormComponent = ({
                 ...child.props,
                 onChange: handleChange,
                 value: data[child.props.name] || "",
-                error: errors[child.props.name]
+                error: errors[child.props.name],
+                onKeyDown: handleKeyDown
             };
         }
         if (childType === "string") {
